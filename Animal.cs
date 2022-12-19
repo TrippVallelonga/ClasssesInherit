4365b823ac1823d9500a71eaa23ef99be384d23f
TrippVallelonga <121043503+TrippVallelonga@users.noreply.github.com>
2022-12-19 10:26:33 -0500

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Animal
+{
+	public string name;
+	public int age;
+	public string color;
+	public string gender;
+	public float weight;
+
+	public Animal()
+	{
+	}
+}
+
+
+
+public class Dog : Animal
+{
+	public string breed;
+	public string ownerName;
+	public bool rabbiesShot;
+
+	public Dog()
+	{
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Program Name: Animal Sorter
+ * Programmer: Tripp Vallelonga
+ * Date: 12/19/2022
+ *
+ * Description: This code makes it so i 
+ * can sort animals into files.
+ */
+
+Animal bob = new Animal();
+
+bob.name = "Bob";
+bob.age = 12;
+bob.gender = "Male";
+bob.color = "Blue and Green";
+bob.weight = 72;
+
+Dog dru = new Dog();
+
+dru.name = "Dru";
+dru.age = 1;
+dru.gender = "Male";
+dru.color = "Black and White";
+dru.weight = 1;
+dru.breed = "Lab Mix";
+dru.ownerName = "Mr. Krabs";
+dru.rabbiesShot = true;
+
+Dog Cbum = new Dog();
+
+dru.name = "Cbum";
+dru.age = 56;
+dru.gender = "Male";
+dru.color = "Spray tan";
+dru.weight = 225;
+dru.breed = "Golden Retriever";
+dru.ownerName = "Patrick";
+dru.rabbiesShot = true;
+
+Dog Gary = new Dog();
+
+dru.name = "Gary";
+dru.age = 12;
+dru.gender = "Male";
+dru.color = "Pink and blue";
+dru.weight = 12;
+dru.breed = "Snail";
+dru.ownerName = "Spongebob";
+dru.rabbiesShot = false;
